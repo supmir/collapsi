ee08d96e72ebc8d3002d10bddd4940f60b2a8b2f
Amir <iskandar.amir@gmail.com>
2025-06-14 00:54:59 +0800

    Make it work

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -39,31 +39,16 @@ export default function Game() {
 
 
     return <div className="">
-        <div className="w-full aspect-square grid grid-cols-6 gap-1">
-            {[...Array(36)].map((_, i) => {
-                return <div key={i} className={`h-full w-full ${[0, 5, 30, 35].includes(i) ? "bg-gray-500" : "bg-gray-700"}`}>
-                    {convertToBoard(i)}
+        <div className="w-full aspect-square grid grid-cols-4 gap-1 p-4">
+            {sample.map((val, i) => {
+                return <div key={i} className={`h-full w-full bg-gray-500 aspect-square`}>
+                    {i}, {val.number}, {val.isCollapsed + ""}
+                    {val.player1?.state === "default" && <div className="w-1/2 aspect-square bg-red-700 mx-auto"></div>}
+                    {val.player2?.state === "default" && <div className="w-1/2 aspect-square bg-blue-700 mx-auto"></div>}
                 </div>;
             })}
 
         </div>
     </div>;
 
-}
-
-
-function convertToBoard(i: number) {
-    if ([0, 5, 30, 35].includes(i)) {
-        return -1;
-    } else if (i < 6) {
-        return i + 24;
-    } else if (i > 29) {
-        return i - 24;
-    } else if (i % 6 == 0) {
-        return i + 4;
-    } else if ((i - 5) % 6 == 0) {
-        return i - 4;
-    } else {
-        return i;
-    }
 }
