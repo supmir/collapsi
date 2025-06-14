7c02036d8b2d436a9864185e6df4b2fd092c54f7
Amir <iskandar.amir@gmail.com>
2025-06-14 21:13:39 +0800

    Animate stuff

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+
 import { GameState, initialiseGameState, PlayerState, updateBoard } from "@/utils/engine";
 import { ArrowDown, ArrowLeft, ArrowRight, ArrowUp, Check, Crown, RotateCcw, Skull } from "lucide-react";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 
 
 
@@ -18,20 +20,42 @@ export default function Game() {
         <div className="landscape:w-2/3 portrait:h-2/3 max-h-screen max-w-screen object-center">
             <div className="aspect-square grid grid-cols-4 gap-1 p-4 m-auto max-h-full max-w-full">
                 {gameState && gameState.board.map((val, i) => {
-                    return <div key={i} className={`aspect-square flex
-                         ${val.type === "collapsed" && "bg-gray-900"}
-                     ${val.type === "path" && "bg-gray-500"}
-                     ${val.type === "default" && "bg-gray-500"}`
-                    }>
-                        <div className="p-2 absolute font-black text-2xl">
-                            {(val.type === "default" || val.type === "path") && val.number}
-
-                        </div>
-                        {val.type === "path" && gameState.player1.displayedPosition !== i && gameState.player2.displayedPosition !== i &&
-                            <div className="w-1/2 aspect-square bg-gray-700 m-auto rounded-full flex items-center justify-center">
-                            </div>}
-                        {gameState.player1.displayedPosition === i && <PlayerPiece playerState={gameState.player1} className="bg-red-700" />}
-                        {gameState.player2.displayedPosition === i && <PlayerPiece playerState={gameState.player2} className="bg-blue-700" />}
+                    return <div
+                        key={i} className={`aspect-square flex relative -z-30
+                         ${val.type === "collapsed" && "bg-gray-900"}`
+                        }>
+                        <AnimatePresence>
+                            {(val.type === "default" || val.type === "path") && <motion.div
+                                className="absolute h-full w-full bg-gray-500 -z-20"
+                                // initial={{ opacity: 0, scale: 0 }}
+                                // animate={{ opacity: 1, scale: 1 }}
+                                exit={{ opacity: 0, scale: 0 }}
+                                transition={{
+                                    duration: 0.4,
+                                    scale: { type: "spring", visualDuration: 2, bounce: 0.5 },
+                                }}
+                            >
+                                <div className="font-black text-2xl p-1">
+                                    {(val.type === "default" || val.type === "path") && val.number}
+                                </div>
+                            </motion.div>}
+                        </AnimatePresence>
+                        <AnimatePresence initial={false}>
+                            {val.type === "path" && gameState.player1.displayedPosition !== i && gameState.player2.displayedPosition !== i &&
+                                <motion.div
+                                    initial={{ opacity: 0, scale: 0 }}
+                                    animate={{ opacity: 1, scale: 1 }}
+                                    exit={{ opacity: 0, scale: 0 }}
+                                    transition={{
+                                        duration: 0.4,
+                                        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+                                    }}
+                                    className="w-1/2 aspect-square bg-gray-700 m-auto rounded-full flex items-center justify-center"
+                                >
+                                </motion.div>}
+                            {gameState.player1.displayedPosition === i && <PlayerPiece playerState={gameState.player1} className="bg-red-700 -z-10" />}
+                            {gameState.player2.displayedPosition === i && <PlayerPiece playerState={gameState.player2} className="bg-blue-700 -z-10" />}
+                        </AnimatePresence>
                     </div>;
                 })}
             </div>
@@ -98,7 +122,28 @@ interface PlayerPieceProps {
 function PlayerPiece(props: PlayerPieceProps) {
     const { playerState: player, className } = props;
 
-    return <div className={`w-2/3 aspect-square m-auto flex`}>
+    return <motion.div
+        className={`w-2/3 aspect-square m-auto flex`}
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0 }}
+
+        transition={{
+            duration: 0.4,
+            scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+        }}
+
+    // key={"player" + className}
+    // layout
+    // transition={
+    //     {
+    //         type: "spring",
+    //         damping: 20,
+    //         stiffness: 300,
+    //     }
+
+    // }
+    >
         <div className={`m-auto p-1 sm:p-2 xl:p-4 rounded-full ${className} w-3/4 h-3/4`}>
             {player.state === "winner" &&
                 <Crown className="h-full w-full" />
@@ -113,5 +158,5 @@ function PlayerPiece(props: PlayerPieceProps) {
                 </div>
             }
         </div>
-    </div>;
+    </motion.div>;
 }
