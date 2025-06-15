3e5450e26ec98e9a0f8321d3aabf67f92dc7e159
Amir <iskandar.amir@gmail.com>
2025-06-15 12:14:11 +0800

    Generate legal moves every update

--- utils/engine.ts
+++ utils/engine.ts
@@ -32,8 +32,11 @@ export interface GameState {
     player2: PlayerState,
     history: PlayerAction[][];
     phase: "play" | "end";
+    validMoves: ValidMoves;
 
 }
+type ValidMoves = { [key: number]: PlayerAction; };
+
 function shuffleTuple<T extends unknown[]>(tuple: readonly [...T]): [...T] {
     return [...tuple]
         .map(value => ({ value, sort: Math.random() }))
@@ -75,27 +78,31 @@ const sample: BoardState = [
 
 export function initialiseGameState(turn: Player): GameState {
     const tempBoard = shuffleTuple(sample);
+    const player1StartingPos = tempBoard.findIndex(obj => obj.number === -1);
+    const player2StartingPos = tempBoard.findIndex(obj => obj.number === -2);
+
     return {
         board: tempBoard,
         turn: turn,
         player1: {
             id: 1,
-            displayedPosition: tempBoard.findIndex(obj => obj.number === -1),
-            currentPosition: tempBoard.findIndex(obj => obj.number === -1),
+            displayedPosition: player1StartingPos,
+            currentPosition: player1StartingPos,
             steps: 4,
             state: "start",
             actions: []
         },
         player2: {
             id: 2,
-            displayedPosition: tempBoard.findIndex(obj => obj.number === -2),
-            currentPosition: tempBoard.findIndex(obj => obj.number === -2),
+            displayedPosition: player2StartingPos,
+            currentPosition: player2StartingPos,
             steps: 4,
             state: "start",
             actions: []
         },
         history: [],
-        phase: "play"
+        phase: "play",
+        validMoves: getLegalMoves(tempBoard, player1StartingPos)
     };
 }
 
@@ -105,7 +112,8 @@ export function updateBoard(current: GameState, action: PlayerAction): GameState
 
     const currentPlayerState = current[targetPlayer];
     const opponenetPlayerState = current[opponenetPlayer];
-    console.log(action);
+    const ret = { ...current };
+
     switch (action) {
         case "up":
         case "down":
@@ -120,15 +128,15 @@ export function updateBoard(current: GameState, action: PlayerAction): GameState
                     throw new Error("Card passed");
                 }
                 current.board[newPosition].type = "path";
-                return {
-                    ...current,
-                    [targetPlayer]: {
-                        ...currentPlayerState,
-                        steps: currentPlayerState.steps - 1,
-                        displayedPosition: newPosition,
-                        actions: [...currentPlayerState.actions, action]
-                    },
+                ret[targetPlayer] = {
+                    ...currentPlayerState,
+                    steps: currentPlayerState.steps - 1,
+                    displayedPosition: newPosition,
+                    actions: [...currentPlayerState.actions, action]
                 };
+                if (currentPlayerState.steps > 1)
+                    ret["validMoves"] = getLegalMoves(current.board, newPosition);
+                return ret;
             }
             throw new Error("Out of steps");
 
@@ -145,14 +153,16 @@ export function updateBoard(current: GameState, action: PlayerAction): GameState
                         ...currentPlayerState,
                         state: isGameEnd ? "winner" : "default",
                         currentPosition: currentPlayerState.displayedPosition,
-                        steps: current.board[currentPlayerState.displayedPosition].number
+                        steps: current.board[currentPlayerState.displayedPosition].number,
+                        actions: [],
                     },
                     [opponenetPlayer]: {
                         ...opponenetPlayerState,
                         state: isGameEnd ? "loser" : opponenetPlayerState.state,
                     },
                     history: [...current.history, currentPlayerState.actions],
-                    phase: isGameEnd ? "end" : "play"
+                    phase: isGameEnd ? "end" : "play",
+                    validMoves: getLegalMoves(current.board, opponenetPlayerState.currentPosition)
                 };
             }
             throw new Error("Not enough steps");
@@ -194,6 +204,19 @@ function getNewPosition(position: number, action: PlayerAction): number {
 
 function isMoveValid(board: BoardState, position: number, action: PlayerAction) {
     return board[getNewPosition(position, action)].type === "default";
+}
+
+function getLegalMoves(board: BoardState, position: number) {
+    const directions: PlayerAction[] = ["up", "down", "left", "right"];
+
+    const ret: ValidMoves = {};
+    directions.forEach(action => {
+        const pos = getNewPosition(position, action);
+        if (board[pos].type === "default") {
+            ret[pos] = action;
+        }
+    });
+    return ret;
 }
 
 function isLegalMoveExist(board: BoardState, position: number, steps: number): boolean {
