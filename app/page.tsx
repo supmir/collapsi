661c26564e06a87eb465d3632cdd97949bc11d21
Amir <iskandar.amir@gmail.com>
2025-06-16 18:38:40 +0800

    Remove tojson

--- app/page.tsx
+++ app/page.tsx
@@ -108,7 +108,7 @@ export default function Game() {
                 pc.onicecandidate = (e) => {
                     console.log("New Offer Ice Candidate");
                     console.log(e.candidate?.candidate);
-                    if (e.candidate) addDoc(offerCandidatesRef, { ...e.candidate, ttl: Timestamp.fromDate(new Date(Date.now() + 60 * 60 * 1000)) }.toJSON());
+                    if (e.candidate) addDoc(offerCandidatesRef, { ...e.candidate, ttl: Timestamp.fromDate(new Date(Date.now() + 60 * 60 * 1000)) });
 
                 };
 
@@ -175,7 +175,7 @@ export default function Game() {
                     pc.onicecandidate = (e) => {
                         console.log("New Answer Ice Candidate");
                         console.log(e.candidate?.candidate);
-                        if (e.candidate) addDoc(answerCandidatesRef, { ...e.candidate, ttl: Timestamp.fromDate(new Date(Date.now() + 60 * 60 * 1000)) }.toJSON());
+                        if (e.candidate) addDoc(answerCandidatesRef, { ...e.candidate, ttl: Timestamp.fromDate(new Date(Date.now() + 60 * 60 * 1000)) });
                     };
 
                     const roomDoc = await getDoc(roomRef);
